41a4bffa6ab85ee649bdbe77bdde82af9c66ee0b
meirber <marb051297@gmail.com>
2023-08-16 16:51:22 +0300

    16-08-23

--- app.js
+++ app.js
@@ -14,10 +14,19 @@ const data = [{
     password: '13572469'
 }]
 
-const { log } = require('console');
 const express = require('express');
 const app = express();
 const port = 3001;
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const passwordValidator = require('password-validator');
+const schema = new passwordValidator();
+schema
+    .is().min(8)
+    .has().uppercase()
+    .has().lowercase();
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -33,12 +42,23 @@ app.get('/:id', (req, res) => {
 
 app.post('/User-creation', (req, res) => {
     const newUser = req.body;
-    const user = data.filter((element) =>
-        element.id === newUser.id);
-    if (user.length !== 0) res.send("User already exists");
+    if (!validator.isEmail(newUser.email))
+        res.send(`${newUser.email} is a valid email address.`);
+    else if (!schema.validate(newUser.password))
+        res.send(`${newUser.password} is not a suitable password.`);
     else {
-        data.push(newUser)
-        res.send("User creation complete")
+        const user = data.filter((element) =>
+            element.id === newUser.id);
+        if (user.length !== 0) res.send("User already exists");
+        else {
+            passwordSecurity(newUser.password)
+                .then((passwordHash) => {
+                    newUser.password = passwordHash;
+                    newUser.uuid = uuidv4();
+                    data.push(newUser);
+                    res.send("User creation complete")
+                })
+        }
     }
 });
 
@@ -61,6 +81,27 @@ app.delete('/delete-user/:id', (req, res) => {
     });
     if (user === 0) res.send("User does not exist");
 })
+
+app.post('/User-login', (req, res) => {
+    const newUser = req.body;
+    data.forEach((element, i) => {
+        if (element.email === newUser.email) {
+            console.log('has email', data[i]);
+            bcrypt.compare(newUser.password, element.password).then(data=>console.log(data))
+        }
+        //  && bcrypt.compare(newUser.password, element.password))
+        // console.log(bcrypt.compare(newUser.password, element.password));
+        // res.send("User is connected");
+    });
+    res.send("wrong credentials");
+}
+);
+
+async function passwordSecurity(password) {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+    return passwordHash;
+}
 
 app.listen(port, () => {
     console.log(`Server is up and running on port:${port}`);
